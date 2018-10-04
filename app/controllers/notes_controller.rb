b38ca434bf695bcd80bdb29bb776544f5385c665
amitleshed <amitleshed@icloud.com>
2018-10-04 18:34:41 +0300

    Validate edit and delete on notes, refactor notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,28 +1,26 @@
 class NotesController < ApplicationController
   
+  before_action :set_notes, only: [:index, :show, :new, :edit]
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @notes = Note.all
   end
 
   def show
-    @note = Note.find(params[:id])
-    @notes = Note.all
   end
 
   def new
     @note = Note.new
-    @notes = Note.all
   end
   
   def edit
-    @note = Note.find(params[:id])
-    @notes = Note.all
   end
   
   def update
-    @note = Note.find(params[:id])
-    @note.update! note
-    redirect_to @note
+    if note_belongs_to_current_user(@note)
+      @note.update! note
+      redirect_to @note
+    end
   end
 
   def create
@@ -31,14 +29,27 @@ class NotesController < ApplicationController
   end
   
   def destroy
-    @note = Note.find(params[:id])
-    @note.destroy!
-    redirect_to notes_path
+    if note_belongs_to_current_user(@note)
+      @note.destroy!
+      redirect_to notes_path
+    end
   end
   
   private
   
     def note
       params.require(:note).permit(:title, :content, :category)
+    end
+    
+    def set_notes
+      @notes = Note.all
+    end
+    
+    def set_note
+      @note = Note.find(params[:id])
+    end
+    
+    def note_belongs_to_current_user(note)
+      current_user.id == note.user.id
     end
 end
